540ee1369afc83311a2a0fe6457df46f046195ea
brahim <57845564+biko2020@users.noreply.github.com>
2021-07-08 12:02:17 +0100

    finish program2

--- program2.py
+++ program2.py
@@ -3,29 +3,45 @@ import os
 import numpy as np 
 import pandas as pd
 import seaborn as sns
+import sys
+
+# importe les donnees de dataset depuis le fichier  diabetes.csv
+for dirname, _, filenames in os.walk('/path/dir'):
+     for filename in filenames:
+        dataset = pd.read_csv('/path/file')
+
+# initialiser les parametres de la fonction g()
+bias = 4
+w1 = 3
+w2 = 2
 
 
-for dirname, _, filenames in os.walk('../bimbiko/Downloads/ML-TP'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+def g(bias,w1,w2,x):
 
-df = pd.read_csv('../bimbiko/diabetescsv/diabetes.csv')
-#df.describe()
-df.shape()
-
+   for X in range(0,5):
+       if X != 0:
+          x = 5
+          return x
 
 def plotData(dataset):
+ 
+    c0=dataset[:,2]==0
+    c1=dataset[:,2]==1
 
-     c0=dataset[:,2]==0
-     c1=dataset[:,2]==1
-     XD1=dataset[:,0][c0]
-     YD1=dataset[:,1][c0]
-     XD2=dataset[:,0][c1]
-     YD2=dataset[:,1][c1]
-     
-     plt.plot(XD1,YD1, 'gD')
-     plt.plot(XD2,YD2, 'bo')
-     plt.plot([0,g(bias,w1,w2,0)],[0,g(bias,w1,w2,5)])
-    
-     #plt.matshow()
-     plt.show(dataset)
+    XD1=dataset[:,0][c0]
+    YD1=dataset[:,1][c0]
+
+    XD2=dataset[:,0][c1]
+    YD2=dataset[:,1][c1]
+
+    plt.plot(XD1,YD1, 'ro')
+    plt.plot(XD2,YD2, 'ro')
+
+plt.plot([0,g(bias,w1,w2,0)],[0,g(bias,w1,w2,5)])
+plt.show() # affiche la figure a l'ecran
+   
+
+if __name__ ==  "__main__ ":
+
+   plotData(sys.dataset)
+  
